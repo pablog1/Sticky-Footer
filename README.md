2532657cdb18a7da2ed99c6a3ebf95e4ba707895
Pablo Goldstoff <p.goldstoff@gmail.com>
2020-09-08 10:30:29 -0300

    first working version

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# JS/JQuery Multi Countdown
+# JS/JQuery Sticky Footer
 
-Multiple instances<br>
-No init is needed<br>
-Two entry modes, one for very simples countdowns, and other for cases where more CSS is needed.<br>
-Pluralization<br>
-Time zones<br>
-Timer Mode<br>
-Configurable Zero Padding
+Height or class triggers
--- sticky-footer.js
+++ sticky-footer.js
@@ -2,8 +2,41 @@
 
 $(function () {
 
-    // Sticky Footer
+  // !Sticky Footer
+  var footerOffset = 800;
+  var footerOffset2 = 2700;
+  var footerStatus = false;
+  var maxWidth = 480;
 
+  if (window.innerWidth <= maxWidth) {
+    $(".sticky-footer").css('display', 'none');
+    $(window).scroll(function () {
+      console.log($(this).scrollTop());
+      //height > footerOffset AND < footerOffset2
+      if (footerStatus == false && ($(this).scrollTop() > footerOffset && $(this).scrollTop() < footerOffset2)) {
+        $(".sticky-footer").slideDown(400);
+        footerStatus = true;
+      } 
+        //height < footerOffset OR > footerOffset2
+        else if (footerStatus == true && ($(this).scrollTop() < footerOffset || $(this).scrollTop() > footerOffset2)) {
+        $(".sticky-footer").slideUp(400);
+        footerStatus = false;
+      }
+    });
+  }
+
+  $(window).resize(function(){ //resize control
+    //console.log('resize');
+    if (window.innerWidth > maxWidth){
+      $(".sticky-footer").css('display', 'none');
+    } else {
+      if (footerStatus == true) {
+        $(".sticky-footer").slideDown(400);
+      } else { // if false
+        $(".sticky-footer").slideUp(400);
+      }
+    }
+  });
    
 
 })
